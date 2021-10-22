b0b1941eaff2521935906f45f3d2a1b14e2b6d88
刘文卓 <374138966@qq.com>
2021-10-22 19:55:43 +0800

    测试

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("helloworld");
-        System.out.println("1234567");
+        System.out.println("123456");
     }
 }
